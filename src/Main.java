51415b71debd95303f0f8d3b9b5b84a7401de565
student <joanna.radom12@gmail.com>
2025-02-25 18:11:09 +0100

    lab1

--- src/Main.java
+++ src/Main.java
@@ -4,8 +4,9 @@ public class Main {
     public static void main(String[] args) {
         Point p1=new Point(); // wywolanie konstruktora w nawiasach podajemy od razu dane do przekazania w zmiennych klasy
         p1.x=5;
-        p1.y=6;    //to co w cpp
-        System.out.println("Twoj x to " + p1.x + " a twoj y to " + p1.y); //taki cout, + to takie << w cout
+        p1.y=6;    //to cout w cpp
+        //System.out.println("Twoj x to " + p1.x + " a twoj y to " + p1.y); //taki cout, + to takie << w cout
+        System.out.println(p1);
 
 
     }
--- src/Point.java
+++ src/Point.java
@@ -1,4 +1,8 @@
 public class Point {
     public double x;  //dostep do zmiennej w całym pliku (private-dostepne tylko w danej klasie)
     public double y;
+
+    public String toString(){       //Metoda to taka funkcja dla klasy
+        return "x="+x+" y="+y;
+    }
 }
